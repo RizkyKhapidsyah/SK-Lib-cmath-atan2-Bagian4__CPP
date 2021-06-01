640bf766276ee2c1c7710841acbb38b6f294618a
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-06-01 07:10:39 +0700

    Add project files.

---
+++ SK-Lib-cmath-atan2-Bagian4__CPP/Source.cpp
@@ -0,0 +1,24 @@
+// CPP program to demonstrate the atan2()
+// function when y/x is undefined
+
+#include <iostream>
+#include <cmath>
+#include <conio.h>
+
+/*	Source by GeeksForGeeks 
+	Modified For Learn by Rizky Khapidsyah
+	I.D.E : VS2019
+*/
+
+using namespace std;
+
+int main() {
+	double x = 0.0, y = 10.0, result;
+	result = atan2(y, x);
+
+	cout << "atan2(y/x) = " << result << " radians" << endl;
+	cout << "atan2(y/x) = " << result * 180 / 3.141592 << " degrees" << endl;
+
+	_getch();
+	return 0;
+}
